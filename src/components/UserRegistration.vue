e3e96b35ba5d30aadc79812d945d9b5a3154e9f2
David Pacassi Torrico <dpacassi@gmail.com>
2018-04-03 20:01:45 +0200

    Restructure user registration form

--- src/components/UserRegistration.vue
+++ src/components/UserRegistration.vue
@@ -9,30 +9,30 @@
     <form @submit.prevent="register">
       <div class="form-item">
         <label for="username">Username</label>
-        <input v-model="username" id="username" type="text" />
-      </div>
-      <div class="form-item">
-        <label for="email">Email</label>
-        <input v-model="email" id="email" type="email" />
+        <input v-model="username" id="username" type="text" required="required" />
       </div>
       <div class="form-item">
         <label for="password">Password</label>
-        <input v-model="password" id="password" type="password" />
+        <input v-model="password" id="password" type="password" required="required" />
+      </div>
+      <div class="form-item">
+        <label for="email">Email</label>
+        <input v-model="email" id="email" type="email" required="required" />
       </div>
       <div class="form-item">
         <label for="first_name">First name</label>
-        <input v-model="first_name" id="first_name" type="text" />
+        <input v-model="first_name" id="first_name" type="text" required="required" />
       </div>
       <div class="form-item">
         <label for="last_name">Last name</label>
-        <input v-model="last_name" id="last_name" type="text" />
+        <input v-model="last_name" id="last_name" type="text" required="required" />
       </div>
-      <div class="form-item">
-        <div class="form-actions" style="float: left;">
+      <div class="form-actions">
+        <div class="form-action">
           <input type="submit" value="Register" />
         </div>
-        <div class="form-actions" style="float: left; margin-left: 15px;">
-          <p>Already have an account? <router-link :to="{path: '/login'}">Login</router-link></p>
+        <div class="form-action">
+          <p>Already have an account? <router-link :to="{path: '/login'}">Login</router-link>!</p>
         </div>
       </div>
     </form>
@@ -47,8 +47,8 @@
     data() {
       return {
         username: '',
-        email: '',
         password: '',
+        email: '',
         first_name: '',
         last_name: '',
         error: false,
