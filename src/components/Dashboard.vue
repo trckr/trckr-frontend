d320170b25c37cf5c9066dfe31798734a52931cc
GabrielAnk <gabriel.ankeshian@gmail.com>
2018-04-04 23:24:06 +0200

    started with listing the projects on the dashboard currently the links to the projects are not displayed correctly the project page for a single project has not been done yet TODO inside getprojects(): - loop correctly through json response - redirect to correct projectpage - call this method on creation of page - router-link for projects doesn't work yet

--- src/components/Dashboard.vue
+++ src/components/Dashboard.vue
@@ -2,10 +2,12 @@
   <div class="component component--dashboard">
     <h1>Dashboard</h1>
     <p>Welcome to <em>trckr</em>!</p>
-      <h2>Your Projects</h2>
+    <h2>Your Projects</h2>
     <form @submit.prevent="createProject">
-      <input type="submit" value="Create Project" />
+      <input type="submit" value="Create New Project" />
     </form>
+    <div class="container" id="projectContainer">
+    </div>
   </div>
 </template>
 
@@ -28,6 +30,8 @@
         const token = this.$store.getters.getCurrentUser.token;
         const that = this;
 
+        that.getprojects();
+
         axios.get('https://trckr-api.trvlr.ch/api/ping/', {
           headers: {
             'Authorization': 'JWT ' + token
@@ -40,7 +44,37 @@
       },
       createProject() {
         router.push('/createproject')
-      }
+      },
+      getprojects(){
+        // TODO:
+        // - loop correctly through json response
+        // - redirect to correct projectpage
+        // - call this method on creation of page
+        // - router-link doesn't work yet
+
+        const token = this.$store.getters.getCurrentUser.token;
+        axios.get('https://trckr-api.trvlr.ch/api/project/', {
+          headers: {
+            'Authorization': 'JWT ' + token
+          }
+        })
+          .then(response => {
+
+            for (var count in response.data){
+              var projectname =  response.data[count].name;
+
+              var project = document.createElement("div");
+              project.style.width = "200px";
+              project.style.height = "50px";
+
+              project.innerHTML = "<router-link :to=\"{path: '/dashboard'}\">" + projectname + "</router-link>";
+              document.getElementById("projectContainer").appendChild(project);
+            }
+          })
+          .catch({
+
+          });
+      },
     }
   }
 </script>
