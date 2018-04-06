de86209e81617a9452e29374ca90d54bff88e0b4
David Pacassi Torrico <dpacassi@gmail.com>
2018-04-06 11:24:11 +0200

    Write a first frontend unit test

---
+++ test/UserRegistration.spec.js
@@ -0,0 +1,11 @@
+import { shallow } from '@vue/test-utils'
+import UserRegistration from '../src/components/UserRegistration.vue'
+
+describe('UserRegistration.vue', function() {
+  it('Testing UserRegistration', function() {
+    const wrapper = shallow(UserRegistration)
+    const message = wrapper.find('h1').text()
+
+    expect(message).toBe('User registration')
+  })
+})
