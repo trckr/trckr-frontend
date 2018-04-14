2cd12bf8b2fc693c154b675455832caff3cb5c55
David Pacassi Torrico <dpacassi@gmail.com>
2018-04-14 14:16:51 +0200

    Fix unit testing <router-link> warning

--- test/UserRegistration.spec.js
+++ test/UserRegistration.spec.js
@@ -1,5 +1,9 @@
 import { shallow } from '@vue/test-utils';
+import Vue from 'vue'
+import Router from 'vue-router';
 import UserRegistration from '../src/components/UserRegistration.vue';
+
+Vue.use(Router);
 
 describe('UserRegistration.vue', function() {
   it('Testing UserRegistration', function() {
