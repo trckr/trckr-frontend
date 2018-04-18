2e7cb6ea6a9c9e56c1f43504b66767622bbade62
David Pacassi Torrico <dpacassi@gmail.com>
2018-04-18 23:38:18 +0200

    Add invalid API call to user login unit tests

--- test/unit/specs/UserLogin.spec.js
+++ test/unit/specs/UserLogin.spec.js
@@ -10,29 +10,53 @@ jest.mock('@/api/token-auth', function() {
   return {
     tokenAuth: {
       post: function(username, password, success, error) {
-        let response = {
-          data: {
-            token: 'secret-token',
-          }
-        };
-        success(response);
+        if (username === 'jest' && password === 'unit-testing') {
+          let response = {
+            data: {
+              token: 'secret-token',
+            },
+          };
+
+          success(response);
+        } else {
+          let response = {
+            data: {
+              non_field_errors: ["Unable to log in with provided credentials."],
+            },
+          };
+
+          error(response);
+        }
       },
     },
   };
 });
 
 describe('UserLogin.vue', function() {
-  let wrapper;
+  let wrapper = shallow(UserLogin, { localVue, store });
 
   beforeEach(function() {
-    wrapper = shallow(UserLogin, { localVue, store });
     jest.resetModules();
     jest.clearAllMocks();
   });
 
-  it('Testing UserLogin', function() {
-    wrapper.find('#username').element.value = 'admin';
-    wrapper.find('#password').element.value = '1234';
+  it('Testing invalid API call [/api/token-auth]', function() {
+    wrapper.setData({
+      username: 'invalid',
+      password: 'login',
+    });
     wrapper.find('form').trigger('submit');
-  })
+
+    expect(localStorage.length).toBe(0);
+  });
+
+  it('Testing valid API call [/api/token-auth]', function() {
+    wrapper.setData({
+      username: 'jest',
+      password: 'unit-testing',
+    });
+    wrapper.find('form').trigger('submit');
+
+    expect(localStorage.trckrCurrentUser).toBe('{"username":"jest","token":"secret-token"}');
+  });
 });
