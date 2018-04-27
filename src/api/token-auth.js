df76fe8b69223b12a4985a41ab9159fff2ec4ceb
GabrielAnk <gabriel.ankeshian@gmail.com>
2018-04-27 15:36:29 +0200

    added shlashes back to the end of rest call urls

--- src/api/token-auth.js
+++ src/api/token-auth.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const apiTokenAuth = {
   post: function(host, username, password, success, error) {
-    axios.post(host + '/api/token-auth', {
+    axios.post(host + '/api/token-auth/', {
       username: username,
       password: password,
     }).then(success).catch(error);
