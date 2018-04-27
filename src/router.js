8ef2c86c5423f180bf027c7e5bd772ff35583cce
GabrielAnk <gabriel.ankeshian@gmail.com>
2018-04-27 15:32:51 +0200

    renamed variable for route

--- src/router.js
+++ src/router.js
@@ -58,7 +58,7 @@ export default new Router({
       requiresAuth: true,
     }
   }, {
-    path: '/project/:projectId/task/:taskid',
+    path: '/project/:projectId/task/:taskId',
     name: 'task page',
     component: TaskPage,
     meta: {
