c0d81d45666c58bc5ee0b9bd65fd8f31ad966df1
David Pacassi Torrico <dpacassi@gmail.com>
2018-05-17 15:40:00 +0200

    Add a basic responsive navigation

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="component component--navigation">
-    <span id="responsive-menu-trigger">
+    <span id="responsive-menu-trigger" v-on:click="toggleResponsiveMenu">
       <svg width="64" height="64" viewBox="0 0 64 64" xmlns="http://www.w3.org/2000/svg">
         <g>
           <path fill="none" stroke="#fff" stroke-width="5" stroke-linejoin="bevel" d="m 5.0916789,20.818994 53.8166421,0" />
@@ -10,7 +10,7 @@
       </svg>
     </span>
 
-    <nav>
+    <nav id="main-navigation">
       <ul>
         <li v-if="!this.$store.getters.isLoggedIn"><router-link to="/login">Login</router-link></li>
         <li v-if="!this.$store.getters.isLoggedIn"><router-link to="/register">Register</router-link></li>
@@ -24,7 +24,29 @@
 </template>
 
 <script>
+  import Snap from 'snapsvg';
+
   export default {
     name: 'Navigation',
+    methods: {
+      toggleResponsiveMenu() {
+        let nav = document.getElementById('main-navigation');
+        let menuTrigger = Snap('#responsive-menu-trigger svg');
+
+        if (nav.classList.contains('is-active')) {
+          menuTrigger.select('path:nth-child(1)').animate({path: 'm 5.0916789,20.818994 53.8166421,0'}, 250);
+          menuTrigger.select('path:nth-child(2)').animate({opacity: 1}, 250);
+          menuTrigger.select('path:nth-child(3)').animate({path: 'm 5.0916788,42.95698 53.8166422,0'}, 250);
+
+          nav.classList.remove('is-active');
+        } else {
+          menuTrigger.select('path:nth-child(1)').animate({path: 'M 12.972944,50.936147 51.027056,12.882035'}, 250);
+          menuTrigger.select('path:nth-child(2)').animate({opacity: 0}, 250);
+          menuTrigger.select('path:nth-child(3)').animate({path: 'M 12.972944,12.882035 51.027056,50.936147'}, 250);
+
+          nav.classList.add('is-active');
+        }
+      },
+    },
   }
 </script>
